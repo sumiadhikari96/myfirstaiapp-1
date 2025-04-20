d30cfc92e9d017a672962962f8ae8d21747f4328
sumiadhikari96 <sumiadhikari96@gmail.com>
2025-04-20 16:42:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,29 +9,29 @@ os.environ['GOOGLE_API_KEY'] = st.secrets['GOOGLE_API_KEY']
 
 # Create prompt template for generating tweets
 
-tweet_template = "Give me {number} tweets on {topic}"
+HR_Metrics_template = "Give me {number} HR Metrics on {topic}"
 
-tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['number', 'topic'])
+Hr_Metrics_prompt = PromptTemplate(template = HR_Metrics_template, input_variables = ['number', 'topic'])
 
 # Initialize Google's Gemini model
 gemini_model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash-latest")
 
 
 # Create LLM chain using the prompt template and model
-tweet_chain = tweet_prompt | gemini_model
+HR_Metrics_chain = HR_Metrics_prompt | gemini_model
 
 
 import streamlit as st
 
-st.header("Tweet Generator - SATVIK")
+st.header("HR Metrics Generator - Sumedha")
 
-st.subheader("Generate tweets using Generative AI")
+st.subheader("Generate HR Metrics using Generative AI")
 
 topic = st.text_input("Topic")
 
 number = st.number_input("Number of tweets", min_value = 1, max_value = 10, value = 1, step = 1)
 
 if st.button("Generate"):
-    tweets = tweet_chain.invoke({"number" : number, "topic" : topic})
-    st.write(tweets.content)
+    HR_Metrics = HR_Metrics_chain.invoke({"number" : number, "topic" : topic})
+    st.write(HR_Metrics.content)
     
